2a33bc7067b07134d423e245e22b8464b02c905e
Rafe-kiho <64768348+Rafe-kiho@users.noreply.github.com>
2024-02-29 00:24:03 +0900

    install axios
    install axios

--- gabozang/src/views/example/04GridData.vue
+++ gabozang/src/views/example/04GridData.vue
@@ -68,13 +68,13 @@ export default {
 </script>
 <style>
 table {
-  border: 2px solid #42b983;
+  border: 2px solid #DFD6BF;
   border-radius: 3px;
   background-color: #fff;
 }
 
 th {
-  background-color: #42b983;
+  background-color: #DFD6BF;
   color: rgba(255, 255, 255, 0.66);
   cursor: pointer;
   user-select: none;
