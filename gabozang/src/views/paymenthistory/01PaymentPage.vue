36999500981dc78fee6ba2af82be213de6c15c5d
Rafe-kiho <64768348+Rafe-kiho@users.noreply.github.com>
2024-03-06 02:33:51 +0900

    정산 메인창 작성

--- gabozang/src/views/paymenthistory/01PaymentPage.vue
+++ gabozang/src/views/paymenthistory/01PaymentPage.vue
@@ -1,23 +1,36 @@
 <template>
-    <div class="body">
-        <p>정산 페이지입니당</p>
+    <div class="outer-box">
+        <div class="jeoungsan-show-table">
+            <PaymentTable></PaymentTable>
+        </div>
     </div>
-
 </template>
 
 <script>
+import PaymentTable from './PaymentTable.vue';
 export default {
+    components: {
+        PaymentTable
+    }
 }
 </script>
 
 <style scoped>
+.outer-box{
+    top:15%;
+    left: 10%;
+}
+.jeoungsan-show-table{
+    top: 5em;
+}
+
+
 .body {
     width: 100%;
     height: 100%;
     margin: 0;
     padding: 0;
     background-color: #EFEAE6;
-    /* 배경 색 설정 */
     font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 }
-</style>
+</style>./PaymentGrid.vue
